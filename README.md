308e326802964b745d42d36266a3979f7f72f284
Jongmin Kim <jongmin.kim@legitgames.net>
2024-08-09 18:38:13 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Shake count App
+
+This app uses the Shake library to count when you shake your phone.
+
+## Getting Started
+
+https://pub.dev/packages/shake is the library used to detect the shake.
+
+## Screenshots
+
+![screenshot_phone.png](github_images%2Fscreenshot_phone.png)
+![screenshot_motion.png](github_images%2Fscreenshot_motion.png)
---
+++ lib/my_home_page.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+
+  @override
+  void initState() {
+    ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
+      _incrementCounter();
+    },
+        shakeThresholdGravity: 1.5
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Make Count up with shake!',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
